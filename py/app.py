3b243c80f4020c16bbcad29e9c037e4aa384818e
Albert Sheu <albert@makenotion.com>
2015-08-20 23:08:28 -0700

    Add application server

---
+++ py/app.py
@@ -0,0 +1,33 @@
+import json
+import functools
+from flask import Flask, Response
+from foxgami.red import Story
+
+app = Flask(__name__)
+
+
+def return_as_json(inner_f):
+    @functools.wraps(inner_f)
+    def new_f(*args, **kwargs):
+        result = inner_f(*args, **kwargs)
+        return Response(json.dumps(
+            result,
+            indent=4,
+            separators=(', ', ': ')
+            ), mimetype='application/json')
+    return new_f
+
+
+@app.route('/api/stories')
+@return_as_json
+def hardcoded_aww():
+    return Story.find()
+
+
+@app.route('/api/stories/<string:story_id>')
+def get_story(story_id):
+    return Story.get(story_id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
